29700ab9a07a5e9dee11c281fe34dd3bb4cf3231
MCTzOCK <dev@mctzock.de>
2021-02-15 19:41:23 +0100

    7th commit

--- src/backend/project/maven/createMaven.js
+++ src/backend/project/maven/createMaven.js
@@ -1,12 +1,16 @@
 const http = require('http')
-const fs = require('fs');
-const path = require("path");
+const fs = require('fs')
+const zlib = require('zlib')
+const unzip = zlib.createUnzip()
 
 function downloadMaven(){
     fs.mkdirSync("./tmp");
     fs.mkdirSync("./tmp/mvn");
     let f = fs.createWriteStream("./tmp/mvn/maven.zip")
-    http.get("https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip", function (response){
-        response.pipe(f);
+    http.get("http://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip", function (response){
+        response.pipe(f)
+        let folder = fs.createReadStream("./mvn/")
+        folder.pipe(unzip).pipe(fs.createWriteStream("./mvn/"));
     });
 }
+exports.downloadMaven = downloadMaven;
