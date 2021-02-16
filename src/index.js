036341d4ab3d6c14f0334232e7610c7d9c34d851
FreshVlay <jonas.kassl@bkf.at>
2021-02-16 08:07:00 +0100

    change home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -12,6 +13,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
   });
+
 
   mainWindow.setMenuBarVisibility(false);
 
